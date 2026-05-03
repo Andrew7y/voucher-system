333c682258438fbf42dadbdb94d2a8ff3f97151c
Kamphaengphet Singkhon <kampang.ks89@gmail.com>
2026-05-03 21:47:56 +0700

    Enable Spring Retry in VoucherSystemApplication

--- src/main/java/com/example/vouchersystem/VoucherSystemApplication.java
+++ src/main/java/com/example/vouchersystem/VoucherSystemApplication.java
@@ -2,8 +2,10 @@ package com.example.vouchersystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
+@EnableRetry
 public class VoucherSystemApplication {
 
     public static void main(String[] args) {
