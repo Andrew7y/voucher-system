51e08d0cee6ea3e39448f5174fc182e7a200aebe
Kamphaengphet Singkhon <kampang.ks89@gmail.com>
2026-05-04 16:03:22 +0700

    Add RedisScript bean for claim voucher Lua script

--- src/main/java/com/example/vouchersystem/config/RedisConfig.java
+++ src/main/java/com/example/vouchersystem/config/RedisConfig.java
@@ -2,8 +2,11 @@ package com.example.vouchersystem.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
+import org.springframework.data.redis.core.script.RedisScript;
 import org.springframework.data.redis.serializer.GenericJacksonJsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 import tools.jackson.databind.json.JsonMapper;
@@ -46,5 +49,13 @@ public class RedisConfig {
 
         redisTemplate.afterPropertiesSet();
         return redisTemplate;
+    }
+
+    @Bean
+    public RedisScript<Long> claimVoucherScript(){
+        DefaultRedisScript<Long> script = new DefaultRedisScript<>();
+        script.setLocation(new ClassPathResource("scripts/claim_voucher.lua"));
+        script.setResultType(Long.class);
+        return script;
     }
 }
