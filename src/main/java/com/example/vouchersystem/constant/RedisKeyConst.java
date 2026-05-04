fd1e888f25eb8c80b14d627178e54e1e64cc0e7d
Kamphaengphet Singkhon <kampang.ks89@gmail.com>
2026-05-04 16:03:39 +0700

    Add claimed users key constant to RedisKeyConst

--- src/main/java/com/example/vouchersystem/constant/RedisKeyConst.java
+++ src/main/java/com/example/vouchersystem/constant/RedisKeyConst.java
@@ -2,10 +2,15 @@ package com.example.vouchersystem.constant;
 
 public class RedisKeyConst {
     public static final String VOUCHER_QUOTA_PREFIX = "voucher:quota:";
+    public static final String VOUCHER_CLAIMED_USERS_PREFIX = "voucher:claimed:";
 
     private RedisKeyConst(){}
 
     public static String getQuotaKey(Long ruleId){
         return VOUCHER_QUOTA_PREFIX + ruleId;
+    }
+
+    public static String getClaimedUsersKey(Long ruleId){
+        return VOUCHER_CLAIMED_USERS_PREFIX + ruleId;
     }
 }
