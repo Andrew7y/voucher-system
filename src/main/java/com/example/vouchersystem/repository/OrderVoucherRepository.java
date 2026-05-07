0b8494910ec548bb7e25a5da957b5982a9a3274b
Kamphaengphet Singkhon <kampang.ks89@gmail.com>
2026-05-08 01:00:26 +0700

    Add OrderVoucherRepository to manage OrderVoucher entities

---
+++ src/main/java/com/example/vouchersystem/repository/OrderVoucherRepository.java
@@ -0,0 +1,12 @@
+package com.example.vouchersystem.repository;
+
+import com.example.vouchersystem.domain.entity.OrderVoucher;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface OrderVoucherRepository extends JpaRepository<OrderVoucher, Long> {
+    Optional<OrderVoucher> findByOrderIdAndUserVoucherId(String orderId, Long userVoucherId);
+}
