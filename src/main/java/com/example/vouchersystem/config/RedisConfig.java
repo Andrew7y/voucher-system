7e98ef395ed67446f33fd0b782618ebb36dd5248
Kamphaengphet Singkhon <kampang.ks89@gmail.com>
2026-05-12 17:00:25 +0700

    Refactor RedisConfig to improve JSON serialization with updated type validation

--- src/main/java/com/example/vouchersystem/config/RedisConfig.java
+++ src/main/java/com/example/vouchersystem/config/RedisConfig.java
@@ -9,44 +9,34 @@ import org.springframework.data.redis.core.script.DefaultRedisScript;
 import org.springframework.data.redis.core.script.RedisScript;
 import org.springframework.data.redis.serializer.GenericJacksonJsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-import tools.jackson.databind.json.JsonMapper;
 import tools.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
 
 @Configuration
 public class RedisConfig {
-    private JsonMapper redisMapper() {
-        return JsonMapper.builder()
-                .activateDefaultTyping(
-                        BasicPolymorphicTypeValidator.builder()
-                                .allowIfBaseType("com.example.vouchersystem")
-                                .allowIfBaseType("java.lang")
-                                .allowIfBaseType("java.time")
-                                .allowIfBaseType("java.util")
-                                .allowIfBaseType("java.math")
-                                .allowIfBaseType("java.util.concurrent")
-                                .allowIfBaseType("java.util.Optional")
-                                .allowIfBaseType("java.util.Record")
-                                .build()
-                )
-                .build();
-    }
-
     @Bean
     public RedisTemplate<String, Object> redisTemplate(
             RedisConnectionFactory connectionFactory
-    ){
-        GenericJacksonJsonRedisSerializer serializer =
-                new GenericJacksonJsonRedisSerializer(redisMapper());
+    ) {
+        var typeValidator = BasicPolymorphicTypeValidator.builder()
+                .allowIfSubType("com.example.vouchersystem")
+                .allowIfSubType("java.lang")
+                .allowIfSubType("java.time")
+                .allowIfSubType("java.util")
+                .allowIfSubType("java.math")
+                .allowIfSubTypeIsArray()
+                .build();
+
+        GenericJacksonJsonRedisSerializer serializer = GenericJacksonJsonRedisSerializer
+                .builder()
+                .enableDefaultTyping(typeValidator)
+                .build();
 
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(connectionFactory);
-
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setHashKeySerializer(new StringRedisSerializer());
-
         redisTemplate.setValueSerializer(serializer);
         redisTemplate.setHashValueSerializer(serializer);
-
         redisTemplate.afterPropertiesSet();
         return redisTemplate;
     }
