284b77ae747c9c07febf162e08f0d914b3196028
Kamphaengphet Singkhon <kampang.ks89@gmail.com>
2026-05-12 17:00:11 +0700

    Refactor CampaignCacheDto to use a class structure with Lombok annotations for better readability and maintainability

--- src/main/java/com/example/vouchersystem/domain/dto/CampaignCacheDto.java
+++ src/main/java/com/example/vouchersystem/domain/dto/CampaignCacheDto.java
@@ -1,12 +1,17 @@
 package com.example.vouchersystem.domain.dto;
 
 import com.example.vouchersystem.domain.entity.CampaignStatus;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
-public record CampaignCacheDto(
-        CampaignStatus status,
-        LocalDateTime startAt,
-        LocalDateTime endAt
-) {
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CampaignCacheDto{
+    private CampaignStatus status;
+    private LocalDateTime startAt;
+    private LocalDateTime endAt;
 }
